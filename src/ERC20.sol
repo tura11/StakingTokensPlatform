d8140c318654af8d437d865a2c61c16140700525
Tymek <tymek_uramek@wp.pl>
2026-02-18 21:16:49 +0100

    ERC20 for contract

---
+++ src/ERC20.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract StakingTokens is ERC20 {
+    
+    address owner;
+
+
+    constructor() ERC20("Token","TK"){
+        owner = msg.sender;
+    }
+
+    function mint(address to, uint256 amount) external {
+        require(msg.sender == owner);
+        _mint(to, amount);
+    }
+
+}
