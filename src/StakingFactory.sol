2d58b5b911c979f4dd380bd02905f65eb6e64238
Tymek <tymek_uramek@wp.pl>
2026-03-10 17:31:29 +0100

    Feat:StakingPool

--- src/StakingFactory.sol
+++ src/StakingFactory.sol
@@ -5,30 +5,35 @@ pragma solidity ^0.8.20;
 
 import {Tura11ERC20} from "./Tura11ERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {StakingPool} from "./StakingPool.sol";
 
-contract StakingFactory {
+contract StakingFactory is Ownable {
 
     error StakingFactory__AddressZero();
     error StakingFactory__PoolAlreadyExists();
 
-    address public s_stakedToken;
     Tura11ERC20 public rewardToken;
     mapping(address => address) public s_pools;
+    address[] public s_allPools;
     
-    
+    event PoolCreated(address stakedToken, address pool);
 
     constructor(address _rewardToken) Ownable(msg.sender) {
         rewardToken = Tura11ERC20(_rewardToken);
     }
 
-    function createPool(address _stakedToken) external onlyOwner {
-        if(s_stakedToken != address(0)) {
+    function createPool(address stakedToken) external onlyOwner returns (address) {
+        if(stakedToken == address(0)) {
             revert StakingFactory__AddressZero();
         }
-        if(s_pools[_stakedToken] != address(0)) {
+        if(s_pools[stakedToken] != address(0)) {
             revert StakingFactory__PoolAlreadyExists();
         }
-        s_stakedToken = _stakedToken;
+        StakingPool pool = new StakingPool(stakedToken, address(rewardToken));
+        s_pools[stakedToken] = address(pool);
+        s_allPools.push(address(pool));
+        emit PoolCreated(stakedToken, address(pool));
+        return address(pool);
     }
 
 
