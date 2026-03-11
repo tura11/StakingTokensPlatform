295b64790c5478c096f519d4ec60933d5e8e348d
Tymek <tymek_uramek@wp.pl>
2026-03-11 19:17:33 +0100

    Withdrawn function tested

--- test/unit/StakingPool.t.sol
+++ test/unit/StakingPool.t.sol
@@ -33,6 +33,11 @@ contract StakingPoolTest is Test {
         stakeToken.mint(owner, 1000);
         rewardToken.mint(owner, 1000);
 
+        vm.startPrank(user1);
+        stakeToken.mint(user1, 10000);
+        stakeToken.approve(address(pool), 10000);
+        vm.stopPrank();
+
 
     }
 
@@ -43,11 +48,11 @@ contract StakingPoolTest is Test {
     }
 
 
-        //STAKE FUNCTION//
+    // ============================================================================
+    // STAKING TESTS
+    // ============================================================================
     function testStake() public {
         vm.startPrank(user1);
-        stakeToken.mint(user1, 1000);
-        stakeToken.approve(address(pool), 1000);
         pool.stake(1000);
         assertEq(1000, stakeToken.balanceOf(address(pool)));
         assertEq(pool.getUserBalance(user1), 1000);
@@ -57,8 +62,6 @@ contract StakingPoolTest is Test {
 
     function testStakeRevertIfAmountIsZero() public {
         vm.startPrank(user1);
-        stakeToken.mint(user1, 1000);
-        stakeToken.approve(address(pool), 1000);
         vm.expectRevert(StakingPool.StakingPool__InvalidAmount.selector);
         pool.stake(0);
 
@@ -66,11 +69,47 @@ contract StakingPoolTest is Test {
 
     function testStakeEmitEvent() public {
         vm.startPrank(user1);
-        stakeToken.mint(user1, 1000);
-        stakeToken.approve(address(pool), 1000);
         vm.expectEmit(true,false,false,true);
         emit  StakingPool.Staked(user1, 1000);
         pool.stake(1000);
+    }
+
+
+    // ============================================================================
+    // WITHDRAW TESTS
+    // ============================================================================
+
+    function testWithdraw() public {
+        vm.startPrank(user1);
+        pool.stake(1000);
+        pool.withdraw(500);
+        assertEq(500, stakeToken.balanceOf(address(pool)));
+        assertEq(pool.getUserBalance(user1), 500);
+        assertEq(pool.getTotalSupply(), 500);
+    }
+
+    function testWithdrawRevertInvalidAmount() public {
+        vm.startPrank(user1);
+        pool.stake(1000);
+        vm.expectRevert(StakingPool.StakingPool__InvalidAmount.selector);
+        pool.withdraw(0);
+    }
+
+
+    function testWithdrawRevertIfBalanceTooLow() public {
+        vm.startPrank(user1);
+        pool.stake(1000);
+        vm.expectRevert(StakingPool.StakingPool__BalanceTooLow.selector);
+        pool.withdraw(1500);
+    }
+
+
+    function testWithdrawEmitEvent() public {
+        vm.startPrank(user1);
+        pool.stake(1000);
+        vm.expectEmit(true,false,false,true);
+        emit  StakingPool.Withdrawn(user1, 500);
+        pool.withdraw(500);
     }
 }
 
