6a75338ba28e7ff11c8f6762d738b2063b5d89b9
Tymek <tymek_uramek@wp.pl>
2026-03-13 08:31:28 +0100

    Owner getter

--- src/interfaces/IStakingFactory.sol
+++ src/interfaces/IStakingFactory.sol
@@ -7,4 +7,5 @@ interface IStakingFactory {
     function createPool(address stakeToken) external returns (address);
     function getPool(address stakeToken) external view returns (address);
     function getAllPools() external view returns (address[] memory);
+    function owner() external view returns (address);
 }
