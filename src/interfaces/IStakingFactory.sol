41c12bdf82df6b54155070a4c34332b56215a980
Tymek <tymek_uramek@wp.pl>
2026-03-13 07:46:14 +0100

    interface for factory

---
+++ src/interfaces/IStakingFactory.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+
+interface IStakingFactory {
+    function createPool(address stakeToken) external returns (address);
+    function getPool(address stakeToken) external view returns (address);
+    function getAllPools() external view returns (address[] memory);
+}
