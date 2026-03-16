59647e89a4927f8d3e4d07c0faa945d3eb730ac3
Tymek <tymek_uramek@wp.pl>
2026-03-16 08:51:28 +0100

    FEAT: HandlerPool.t.sol

--- test/invariants/HandlerPool.t.sol
+++ test/invariants/HandlerPool.t.sol
@@ -2,12 +2,10 @@
 
 pragma solidity ^0.8.20;
 
-
 import {Test, console} from "forge-std/Test.sol";
 import {StakingPool} from "../../src/StakingPool.sol";
 import {Tura11ERC20} from "../../src/Tura11ERC20.sol";
 import {ERC20Mock} from "openzeppelin-contracts/contracts/mocks/token/ERC20Mock.sol";
-
 
 contract Handler is Test {
 
@@ -17,47 +15,73 @@ contract Handler is Test {
 
     address[] users = new address[](2);
     uint256 public totalDeposited;
-    uint256 public totalWithdraw;
-    uint256 public constant MAX_AMOUNT = 10000000 * 1e18;
+    uint256 public totalWithdrawn;
+    uint256 public totalRewardDeposited;
+    uint256 public constant MAX_AMOUNT = 10_000_000 * 1e18;
+    address owner;
 
-    constructor(StakingPool _pool, Tura11ERC20 _rewardToken, ERC20Mock _stakeToken) {
+    constructor(StakingPool _pool, Tura11ERC20 _rewardToken, ERC20Mock _stakeToken, address _owner) {
         pool = _pool;
         rewardToken = _rewardToken;
         stakeToken = _stakeToken;
+        owner = _owner;
         users[0] = makeAddr("user1");
         users[1] = makeAddr("user2");
     }
 
-
     function deposit(uint256 userIndex, uint256 amount) public {
-        address user = users[userIndex % users.length];
-        amount = bound(amount, 0, MAX_DEPOSIT);
+        address user = users[bound(userIndex, 0, users.length - 1)];
+        amount = bound(amount, 1, MAX_AMOUNT);
+
+        stakeToken.mint(user, amount);
 
         vm.startPrank(user);
-        stakeToken.mint(user, amount);
         stakeToken.approve(address(pool), amount);
         pool.stake(amount);
         vm.stopPrank();
 
         totalDeposited += amount;
-
-
     }
 
-
     function withdraw(uint256 userIndex, uint256 amount) public {
-        address user = users[userIndex % users.length];
-        
-        uint256 userBalance = pool.balanceOf(user);
+        address user = users[bound(userIndex, 0, users.length - 1)];
 
-        if(userBalance == 0) {
-            return;
-        }
+        uint256 userBalance = pool.getUserBalance(user);
+        if (userBalance == 0) return;
+
         amount = bound(amount, 1, userBalance);
 
         vm.prank(user);
         pool.withdraw(amount);
 
+        totalWithdrawn += amount;
     }
 
+    function claimReward(uint256 userIndex) public {
+        address user = users[bound(userIndex, 0, users.length - 1)];
+
+        if (pool.earned(user) == 0) return;
+
+        vm.prank(user);
+        pool.claimReward();
+    }
+
+    function notifyRewardAmount(uint256 reward, uint256 duration) public {
+        reward = bound(reward, 1, MAX_AMOUNT);
+        duration = bound(duration, 1, 30 days);
+
+        rewardToken.mint(owner, reward);
+
+        vm.startPrank(owner);
+        rewardToken.approve(address(pool), reward);
+        pool.notifyRewardAmount(reward, duration);
+        vm.stopPrank();
+
+        totalRewardDeposited += reward;
+    }
+
+    function warpTime(uint256 seconds_) public {
+        seconds_ = bound(seconds_, 1, 30 days);
+        vm.warp(block.timestamp + seconds_);
+    }
 }
